f7150d8644fbb4d5101273ccf04ef7a6620deeff
Amelia Elton <ameliareneeelton@gmail.com>
2020-10-21 19:05:12 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -20,11 +20,12 @@ def run_guessing_game
   random_number
   welcome
   user_number = user_input
-  if user_number == random_number
+  compter_number = random_number
+  if user_number == compter_number
     puts "You guessed the correct number!"
   elsif user_number == "exit"
     goodbye 
   else 
-    puts "Sorry! the computer guessed #{random_number}."
+    puts "Sorry! the computer guessed #{compter_number}."
   end
 end
