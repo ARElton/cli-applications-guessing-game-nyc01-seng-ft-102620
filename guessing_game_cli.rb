d5c9dfafe56b471f0bad906121a3a96baebc8019
Amelia Elton <ameliareneeelton@gmail.com>
2020-10-21 18:59:22 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -10,7 +10,18 @@ def user_input
   gets.chomp 
 end
 
+def goodbye
+  puts "Goodbye!"
+end
+
 
 
 def run_guessing_game
+  random_number
+  welcome
+  user_number = user_input
+  if user_input == random_number
+    puts "You guessed the correct number!"
+  else 
+    puts "Sorry! the computer guessed #{random_number}"
 end
